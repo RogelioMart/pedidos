2b0affd082d809eb3a546aa4effd4b27bc949eea
Rogelio Martinez <55266360+RogelioMart@users.noreply.github.com>
2019-12-19 16:07:44 -0700

    Add files via upload
    added a function checks if a number is an integer into toExcel added a README file that specifies what needs to be installed in order to use the program

---
+++ README.txt
@@ -0,0 +1,9 @@
+README
+
+This program is intended to get information and input it into excel
+it also uses a GUI in order to enables these functionalities install the
+following
+
+pip install requests
+
+pip install openpyxl
--- toExcel.py
+++ toExcel.py
@@ -1,9 +1,35 @@
 #used to evaluate data and put it into the excel file
 
+def isInt(mCant):
+	
+	retVal = True
+	
+	for iterator in range(len(mCant)):
+		
+		if (s[iterator].isdigit() != True):
+			
+			iterator = len(mCant) + 2
+			
+			retVal = False
+		
+		#if statement ends here
+	
+	#for loop ends here
 
+	return(retVal)
+
+#ends isInt function
 
 def evaluation(mTipo, mNom, mCant):
 	
 	#print("toExcel got\n" + mTipo + "\n" + mNom + "\n" + mCant) #DEBUGGING
 	
+	isItInt = False
+	
+	isItInt = isInt(mCant)
+	
+	if(isItInt == True):
+		appendToExcel()
+	
 #ends evaluation function
+
